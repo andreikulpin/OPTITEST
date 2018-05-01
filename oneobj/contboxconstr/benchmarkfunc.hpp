178cb946e9ce2cc4973eb8ddad0c254c150c23ef
andrei <andreikulpin@gmail.com>
2018-05-02 02:13:55 +0300

    [upd] add call counter to benchmark functor

--- oneobj/contboxconstr/benchmarkfunc.hpp
+++ oneobj/contboxconstr/benchmarkfunc.hpp
@@ -33,7 +33,7 @@ namespace OPTITEST {
         BenchmarkFunctor(const PtrBench<double> benchmark) : bm(benchmark) {
         }
 
-        double func(const double* x) {
+        double func(const double* x) const {
             std::vector<double> v(x, x+bm->getDim());
             return bm->calcFunc(v);            
         }
@@ -61,8 +61,9 @@ namespace OPTITEST {
 	    int dim = bm->getDim();
             COMPI::MPProblem<double>* prob = new COMPI::MPProblem<double>();
             prob->mVarTypes.assign(dim, COMPI::MPProblem<double>::VariableTypes::GENERIC);
-            std::shared_ptr<COMPI::Functor <double>> pf = std::make_shared<BenchmarkFunctor>(BenchmarkFunctor(bm));
-            prob->mObjectives.push_back(pf);
+            std::shared_ptr<COMPI::Functor <double>> funcPtr = std::make_shared<BenchmarkFunctor>(BenchmarkFunctor(bm));
+            std::shared_ptr<COMPI::Functor <double>> funcCntPtr = std::make_shared<COMPI::FuncCnt<double>>(funcPtr);
+            prob->mObjectives.push_back(funcCntPtr);
             prob->mBox = new snowgoose::Box<double>(dim);
             for (int i = 0; i < dim; i++) {
                 prob->mBox->mA[i] = bm->getBounds()[i].first;
